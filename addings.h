955020f26fdaba56e5c7d4fa0a9c5dac04d5e33c
AshenMoon <152204005+AshenMoon@users.noreply.github.com>
2023-11-29 22:30:38 +0200

    Fix build error. Allow backgroung color selection

--- addings.h
+++ addings.h
@@ -1,82 +1,89 @@
 #ifndef ADDINGS_H
 #define ADDINGS_H
-    
-#if CANDLE_KOL >0
-    ////////////////////////////////////////////////////////////// Свечи
-    DEFINE_GRADIENT_PALETTE( candle_Pal ) {
-      0,   255,  0,  0,   //red
-     90,   255,255,255,  //full white
-    180,   255,255,  0,   //bright yellow
-    255,   255,255,255 }; //full white
-    
-    uint8_t PolCandle=1;    //Положение свечи
-    
-    void addcandle() 
-    {
-      uint16_t poz=PolCandle;
-      CRGBPalette16 myPal = candle_Pal;
-    
-      if (NUM_LEDS > CANDLE_KOL)
-      {
-        uint16_t kol=NUM_LEDS /CANDLE_KOL;      //Количество свечей
-        
-        for ( uint16_t x = 0; x<kol; x++) 
-        {
-          if (poz < NUM_LEDS)
-            leds[ poz ] = ColorFromPalette( myPal, random8(255));
-          poz +=CANDLE_KOL;
-        }
-      }
+
+#if CANDLE_KOL > 0
+////////////////////////////////////////////////////////////// Свечи
+DEFINE_GRADIENT_PALETTE(candle_Pal){
+  0, 255, 0, 0,       //red
+  90, 255, 255, 255,  //full white
+  180, 255, 255, 0,   //bright yellow
+  255, 255, 255, 255
+};  //full white
+
+uint8_t PolCandle = 1;  //Положение свечи
+
+void addcandle() {
+  uint16_t poz = PolCandle;
+  CRGBPalette16 myPal = candle_Pal;
+
+  if (NUM_LEDS > CANDLE_KOL) {
+    uint16_t kol = NUM_LEDS / CANDLE_KOL;  //Количество свечей
+
+    for (uint16_t x = 0; x < kol; x++) {
+      if (poz < NUM_LEDS)
+        leds[poz] = ColorFromPalette(myPal, random8(255));
+      poz += CANDLE_KOL;
     }
+  }
+}
 #endif
 
 ///////////////////////////////////////////////////////////////// Черный фон
-void addbackground() 
-{
-  #if MAX_LEDS < 255
-    uint8_t i; 
-  #else
-    uint16_t i;
-  #endif
+void addbackground() {
+#if MAX_LEDS < 255
+  uint8_t i;
+#else
+  uint16_t i;
+#endif
 
-   for (i = 0; i < NUM_LEDS ; i++ ) 
-    if (  (leds[i].r < 5) &&
-          (leds[i].g < 5) &&
-          (leds[i].b < 5) )
-    leds[i].b +=CRGB(5,5,5);
-  
+  for (i = 0; i < NUM_LEDS; i++)
+    if ((leds[i].r < 5) && (leds[i].g < 5) && (leds[i].b < 5)) {
+#if BACKGR_ON == 3
+      leds[i] += CRGB(5, 0, 0);
+#elif BACKGR_ON == 2
+      leds[i] += CRGB(0, 2, 0);
+#elif BACKGR_ON > 0
+      leds[i] += CRGB(0, 0, 5);
+#endif
+    }
 }
 
 ////////////////////////////////////////////////////////////////// Блеск
-void addglitter( fract8 chanceOfGlitter) 
-{
-  if( random8() < chanceOfGlitter) {
-    #if MAX_LEDS < 255
-      leds[ random8(NUM_LEDS) ] += CRGB::White;
-    #else
-      leds[ random16(NUM_LEDS) ] += CRGB::White;
-    #endif
+void addglitter(fract8 chanceOfGlitter) {
+  if (random8() < chanceOfGlitter) {
+#if MAX_LEDS < 255
+    leds[random8(NUM_LEDS)] += CRGB::White;
+#else
+    leds[random16(NUM_LEDS)] += CRGB::White;
+#endif
   }
 }
 
 ////////////////////////////////////////////////////////////////// Бенгальский огонь
-void sparkler(uint8_t n)       //Бенгальский огонь
+void sparkler(uint8_t n)  //Бенгальский огонь
 // 0 - нет эффектов
 // 1 - Бенгальский огонь
 // 2 - 1 яркий светодиод
 // 3 - метеорит
-// 4 - случайный эффект 
-{ uint8_t kol=3;  
+// 4 - случайный эффект
+{
+  uint8_t kol = 3;
 
-  if ( KolLed< 10 ) kol = KolLed /3;
-  if (kol>=2)
-  { uint8_t nn = n;
+  if (KolLed < 10) kol = KolLed / 3;
+  if (kol >= 2) {
+    uint8_t nn = n;
     switch (nn) {
-      case 1 :  for ( uint8_t x = 0; x<kol; x++) leds[ KolLed-random8(kol*2) ] = CRGB::White; break;            //Бенгальский
-      case 2 :  leds[KolLed-1] = CRGB::White; break;                                                            //1 яркий
-      case 3 :  leds[KolLed] = CRGB::White;leds[KolLed-1] = CRGB::Red;leds[KolLed-2] = CRGB::Violet; break;     //Метеорит
+      case 1:
+        for (uint8_t x = 0; x < kol; x++) leds[KolLed - random8(kol * 2)] = CRGB::White;
+        break;                                        //Бенгальский
+      case 2: leds[KolLed - 1] = CRGB::White; break;  //1 яркий
+      case 3:
+        leds[KolLed] = CRGB::White;
+        leds[KolLed - 1] = CRGB::Red;
+        leds[KolLed - 2] = CRGB::Violet;
+        break;  //Метеорит
     }
-  }      
+  }
 }
 
 #endif
